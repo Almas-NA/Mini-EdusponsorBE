ceac6e8f4ad8b89e7148fa7e8ecc33c0b5122a2c
Almas-NA <24mp2303@rit.ac.in>
2025-09-04 18:55:08 +0530

    first commit

---
+++ edusponsor/edusponsor/src/main/java/com/example/edusponsor/EdusponsorApplication.java
@@ -0,0 +1,13 @@
+package com.example.edusponsor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusponsorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusponsorApplication.class, args);
+	}
+
+}
---
+++ edusponsor/edusponsor/src/test/java/com/example/edusponsor/EdusponsorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edusponsor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusponsorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
